dfba8af0535206f52f4723ff88a8ece3c9d48c77
MihailMoskovskih <mihailmoskovskih.360p@gmail.com>
2024-05-20 01:54:26 +0300

    Added test.cpp and CMakeLists.txt and built the project

---
+++ test/Adapter_test.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+TEST(PlayerTest, ForwardsAttack) {
+    Forwards f("John");
+    testing::internal::CaptureStdout();
+    f.attack();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("Forward attack John\n", output);
+}
+
+TEST(PlayerTest, CenterDefense) {
+    Center c("Jane");
+    testing::internal::CaptureStdout();
+    c.defense();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("Center defense Jane\n", output);
+}
+
+TEST(PlayerTest, TranslatorAttackAndDefense) {
+    Translator t("YaoMing");
+    testing::internal::CaptureStdout();
+    t.attack();
+    t.defense();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ("Foreign Center attack YaoMing\nForeign Center defense YaoMing\n", output);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
