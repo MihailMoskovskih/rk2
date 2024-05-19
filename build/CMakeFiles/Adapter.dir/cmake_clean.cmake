5831f0e1c097d9088b3ef9ab3a0b51db566b03dd
MihailMoskovskih <mihailmoskovskih.360p@gmail.com>
2024-05-20 01:20:51 +0300

    Added CMakeLists.txt and built the project

---
+++ build/CMakeFiles/Adapter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Adapter"
+  "Adapter.pdb"
+  "CMakeFiles/Adapter.dir/adaper.cpp.o"
+  "CMakeFiles/Adapter.dir/adaper.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Adapter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
