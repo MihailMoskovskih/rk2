e9c771d7494a191e73c0f5dec701ebff7304033f
MihailMoskovskih <mihailmoskovskih.360p@gmail.com>
2024-05-20 02:38:47 +0300

    test try6

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mihail/MihailMoskovskih/rk2/DesignPattern/adapter
+# Build directory: /home/mihail/MihailMoskovskih/rk2/DesignPattern/adapter/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
