0637902f560d3a76b4b86518b8107edb8368aa79
MihailMoskovskih <mihailmoskovskih.360p@gmail.com>
2024-06-06 09:10:10 +0300

    try3

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mihail/MihailMoskovskih/rk2/DesignPattern/adapter/tests
+# Build directory: /home/mihail/MihailMoskovskih/rk2/DesignPattern/adapter/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runTests "/home/mihail/MihailMoskovskih/rk2/DesignPattern/adapter/build/tests/runTests")
+set_tests_properties(runTests PROPERTIES  _BACKTRACE_TRIPLES "/home/mihail/MihailMoskovskih/rk2/DesignPattern/adapter/tests/CMakeLists.txt;22;add_test;/home/mihail/MihailMoskovskih/rk2/DesignPattern/adapter/tests/CMakeLists.txt;0;")
+subdirs("../_deps/googletest-build")
